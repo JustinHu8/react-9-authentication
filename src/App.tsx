3b2c2d408129724017d8e6c07a0ceceec59f223c
Haitao Hu <haitaohu@Haitaos-MacBook-Pro-2.local>
2024-10-14 17:41:30 +1100

    Implementing dynamic routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import HomePage from './pages/HomePage'
 import CourseListPage from './pages/CourseListPage'
+import CourseDetailPage from './pages/CourseDetailsPage'
 import ContactPage from './pages/ContactPage'
 
 
@@ -16,6 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/courses" element={<CourseListPage />} />
+          <Route path="/courses/:courseId" element={<CourseDetailPage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </div>    
---
+++ src/pages/CourseDetailsPage.tsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const CourseDetailsPage = () => {
+  const { courseId } = useParams();
+  return <div>Details for course {courseId}</div>;
+};
+
+export default CourseDetailsPage;
