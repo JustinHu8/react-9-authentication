ce2d2ba3ebffe1817c75e0bc911edea5574460a7
Haitao Hu <haitaohu@Haitaos-MacBook-Pro-2.local>
2024-10-14 17:18:49 +1100

    Clean up commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CourseListPage from './pages/CourseListPage'
 import ContactPage from './pages/ContactPage'
-// import { useCourses } from './hooks/useCourses'
-// import CourseCard from './components/CourseCard/CourseCard'
+
 
 const App = () => {
 
@@ -15,18 +14,7 @@ const App = () => {
       <Route path="/contact" element={<ContactPage />} />
     </Routes>
   )
-  // const { courses, loading, error } = useCourses();
 
-  // if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error: {error}</div>;
-
-  // return (
-  //   <div className="course-list">
-  //     {courses.map(course => (
-  //       <CourseCard key={course.id} {...course} />
-  //     ))}
-  //   </div>
-  // );
 }
 
 export default App
