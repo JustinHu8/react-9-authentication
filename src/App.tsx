f8c0069b112f663b6b2397f3b0cef3bd71a1656d
Haitao Hu <haitaohu@Haitaos-MacBook-Pro-2.local>
2024-10-14 18:09:41 +1100

    Implement protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,30 @@ import ContactPage from './pages/ContactPage'
 import UserProfilePage from './pages/UserProfilePage'
 import Settings from './components/Settings/Settings'
 import Orders from './components/Orders/Orders'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import LoginPage from './pages/LoginPage'
 
+const isAuthenticated = false;
 
 const App = () => {
 
   return (
     <div>
-      <NavBar />
+      <NavBar isAuthenticated={isAuthenticated} />
       <div className="main-content">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/courses" element={<CourseListPage />} />
           <Route path="/courses/:courseId" element={<CourseDetailPage />} />
           <Route path="/contact" element={<ContactPage />} />
-          <Route path="/profile" element={<UserProfilePage />}>
+          <Route path='/login' element={<LoginPage />} />
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute isAuthenticated={isAuthenticated}>
+                <UserProfilePage />
+              </ProtectedRoute>
+            }>
             <Route path="settings" element={<Settings />} />
             <Route path="orders" element={<Orders />} />
           </Route>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
 
-const NavBar = () => (
+interface NavBarProps {
+  isAuthenticated: boolean;
+}
+
+const NavBar = ({ isAuthenticated }: NavBarProps) => (
   <nav className='navbar'>
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/courses">Courses</Link></li>
       <li><Link to="/contact">Contact</Link></li>
+      {isAuthenticated ? (
+        <li><Link to="/profile">Profile</Link></li>
+      ) : (
+        <li><Link to="/login">Login</Link></li>
+      )}
     </ul>
   </nav>
 );
