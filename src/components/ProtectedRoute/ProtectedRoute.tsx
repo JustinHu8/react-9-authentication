a69733d153fde39656b4ff02d48208c8cf090279
Justin Hu <jnelite85@gmail.com>
2024-11-12 11:12:21 +1100

    Update Protected Route

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,12 +1,13 @@
 import { Navigate } from "react-router-dom";
-import { useUser } from '../../context/UserContext';
-
+// import { useUser } from '../../context/UserContext';
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { isAuthenticated } = useUser();
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
   
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
